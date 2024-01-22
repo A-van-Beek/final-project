94ef5fa69ea07ea60ba79a72422d60c83ade0496
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2024-01-22 21:12:52 +0100

    opzet schema.prisma

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.send("Hello world! Welkom in mijn reiswereld");
 });
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+app.listen(3333, () => {
+  console.log("Server is listening on port 3333");
 });

785cdd3f94133c7785be317f77d78e42377ad8ec
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2024-02-18 13:46:46 +0100

    fix errorlogging getBooking

--- services/bookings/getBookings.js
+++ services/bookings/getBookings.js
@@ -1,11 +1,19 @@
 import { PrismaClient } from "@prisma/client";
+import NotFoundError from "../../errors/NotFoundError.js";
 
 const getBookings = async (userId) => {
   const prisma = new PrismaClient();
 
-  return prisma.bookings.findMany({
+  const getBookings = await prisma.bookings.findMany({
     where: { userId },
   });
+  if (!getBookings) {
+    throw new NotFoundError("Booking", userId);
+  }
+  if (getBookings.length === 0) {
+    console.log("geen resultaat voor userId! ", userId);
+  }
+  return getBookings;
 };
 
 export default getBookings;
